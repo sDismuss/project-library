2d94f6698b4da7c71b4a35fecd73a104007698e9
Sergey <dismuss.s@users.noreply.github.com>
2020-01-15 14:09:24 +0300

    Create someFunctions.js
    This is function's set for buttons from library.html

---
+++ project_library/resources/static/js/someFunctions.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#hi').click(function () {
+        alert("Hello, World");
+    });
+
+    $('#showAuthors').click(function () {
+        $.getJSON('/api/authors.json', function(jd) {
+            let i = 0;
+            $('#authors').html('<p> Authors </p>');
+            while(i < jd.length) {
+                $('#authors').append('<p> ID: ' + jd[i].id + '</p>');
+                $('#authors').append('<p>Name: ' + jd[i].name + '</p>');
+                i++;
+            }
+        });
+    });
+});
+
