3c7707a61589524483fce94cf94f0eb4f74f638c
Sergey <dismuss.s@users.noreply.github.com>
2020-01-15 14:18:58 +0300

    Change an Array to an ArrayList

--- project_library/src/com/netcracker/controller/LibraryController.java
+++ project_library/src/com/netcracker/controller/LibraryController.java
@@ -5,18 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class LibraryController {
 
-    Book[] books = new Book[3];
-    List<String> authors;
     @GetMapping("/library")
     public String libraryForm(Model model) {
-        books[0] = new Book(1, "Interesting Book", "Marvelo", "Very interesting book");
-        books[1] = new Book(2, "title", "Printino", "Very sad book");
-        books[2] = new Book(3,"Clever Book","Pichelatti","Very clever book");
+
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book(1, "Interesting Book", "Marvelo", "Very interesting book"));
+        books.add(new Book(2, "Sad Book", "Printino", "Very sad book"));
+        books.add(new Book(3,"Clever Book","Pichelatti","Very clever book"));
         model.addAttribute("books", books);
         return "library";
     }
