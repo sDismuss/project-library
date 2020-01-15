6a28b7fc502f28175f619a58049648c55311c359
Sergey <dismuss.s@users.noreply.github.com>
2020-01-15 14:21:09 +0300

    Some changes

---
+++ project_library/src/com/netcracker/controller/APIController.java
@@ -0,0 +1,20 @@
+package com.netcracker.controller;
+
+import com.netcracker.model.Author;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class APIController {
+
+    @GetMapping("/api/authors.json")
+    public List<Author> getAuthors(){
+        List<Author> authors = new ArrayList<Author>();
+        authors.add(new Author("1","Пушкин"));
+        authors.add(new Author("2","Лермонтов"));
+        return authors;
+    }
+}
